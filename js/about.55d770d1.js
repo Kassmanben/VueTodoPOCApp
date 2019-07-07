63721775d7d7259579c1c1290846df4c71392810
KassmanBen <kassmanben@gmail.com>
2019-07-07 15:50:11 -0700

    POC app

---
+++ js/about.55d770d1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},o=[function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",{staticClass:"about"},[a("h1",[t._v("About")]),a("p",[t._v("POC app made alongside a tutorial, done to learn the basics of Vue")])])}],s=a("2877"),u={},l=Object(s["a"])(u,n,o,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.55d770d1.js.map
